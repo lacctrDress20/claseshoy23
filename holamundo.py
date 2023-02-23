d37079809e90b8c463408cc50671de037ae3fcde
lacctrDress20 <1490244@senati.pe>
2023-02-23 10:46:24 -0500

    reedicion

---
+++ holamundo.py
@@ -0,0 +1,8 @@
+
+a=int(input("ingrese el 2do numero", ))
+b=int(input("ingrese el 1er numero", ))
+
+print("Esta es la suma ",a+b)
+print("Esta es la resta ",a-b)
+print("Esta es la multiplicion ",a*b)
+print("Esta es la divicion ",a/b)
