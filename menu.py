4ce829b25c9353b174a40032011b86211bcfad87
lacctrDress20 <1490244@senati.pe>
2023-02-23 11:41:43 -0500

    defino if y agrego el menu para el desayuno

--- menu.py
+++ menu.py
@@ -2,4 +2,23 @@
 # definimos el "while"
 while True:
     print("La comelona")
-    print("   Menu   ")
+    print("Menu")
+
+opcion="menu" or "Menu"
+
+# Definimos la condicion if"si"
+# colocamos el listado de menu
+
+if opcion=="menu" or "Menu": # colocamos las 2 posibles formas de escritura para acceder al menu
+
+    while true:
+        print("BEBIDAS")
+        print("A| café")
+        print("b| chocolate")
+        print("c| maca")
+        print("d| quinua")
+        print("e| refresco")
+    
+
+
+
