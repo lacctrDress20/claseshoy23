a398de1ccb96ac5babdcc5596400e37b0f85dd21
lacctrDress20 <1490244@senati.pe>
2023-02-23 10:57:52 -0500

    calculador/termino de las operaciones

--- calculadorMejorado.py
+++ calculadorMejorado.py
@@ -13,3 +13,20 @@ while True:
     5) Apagar calculadora
     """)
     opcion = int(input("Elige una opción: ") )     
+
+    if opcion == 1:
+        print(" ")
+        print("RESULTADO: La suma de",n1,"+",n2,"es igual a",n1+n2)
+    elif opcion == 2:
+        print(" ")
+        print("RESULTADO: La resta de",n1,"-",n2,"es igual a",n1-n2)
+    elif opcion == 3:
+        print(" ")
+        print("RESULTADO: El producto de",n1,"*",n2,"es igual a",n1*n2)
+    elif opcion == 4:
+        n1 = float(input("Introduce tu primer número: ") )
+        n2 = float(input("Introduce tu segundo número: ") )
+    elif opcion == 5:
+        break
+    else:
+        print("Opción incorrecta")
