e13687133043a0cbcc3bfe277079b7dfacd56596
lacctrDress20 <125669257+lacctrDress20@users.noreply.github.com>
2023-02-24 10:30:58 -0500

    Add files via upload

---
+++ suma de numeros 2.py
@@ -0,0 +1,26 @@
+print("Hola\nComo tas\tjajaja")
+subproducto=0
+x=0+0
+a=0+1
+b=2+1
+d=4+1
+f=6+1
+g=8+1
+h=10+1
+i=12+1
+j=14+1
+print("0\t0\tsuma = ", (subproducto+x))
+print("1\t0\tsuma = ", (subproducto+a))
+print("2\t1\tsuma = ", (subproducto+b))
+print("3\t2\tsuma = ", (subproducto+d))
+print("4\t3\tsuma = ", (subproducto+f))
+print("5\t4\tsuma = ", (subproducto+g))
+print("6\t5\tsuma = ", (subproducto+h))
+print("7\t6\tsuma = ", (subproducto+i))
+print("8\t7\tsuma = ", (subproducto+j))
+
+
+
+    
+
+
