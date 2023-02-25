27ed052cf2a7058bca008c79c492e516d29094ba
lacctrDress20 <1490244@senati.pe>
2023-02-25 09:24:50 -0500

    se logro

--- Nueva carpeta/tarea2.py
+++ Nueva carpeta/tarea2.py
@@ -1,6 +1,4 @@
 #Desarrolle un algoritmo que muestre el siguiene patron
-for i in range(1):
-    print(i, ('1\t2\3\n4\n5'))    
-    for n in range(2,5):
-        print(n,(i))
-
+for i in range(6):
+    print(str(i) *i)    
+    
