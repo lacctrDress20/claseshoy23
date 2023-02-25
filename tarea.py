d10e0fbe44b5508e29bb91f6aae54ed71ba5079d
lacctrDress20 <1490244@senati.pe>
2023-02-25 08:08:55 -0500

    uso if para probar la condicion

--- tarea.py
+++ tarea.py
@@ -1,7 +1,10 @@
 def es_multiplo(numero,multiplo):
     return numero%multiplo==0
 i=int(input('Numero: '))
-
+if es_multiplo(i, 5):
+    print("Es multiplo de 5")
+else:
+    print("No es múltiplo de 5")
 
 
       
